7778bba58e50b3bfce8c904b8e5e369e77f8e70a
Mario Pabon <me@mariopabon.com>
2016-09-05 14:43:44 -0400

    Setup and cleanup store and fetch mock in tests

--- test/middleware.js
+++ test/middleware.js
@@ -1,11 +1,28 @@
 import test from 'ava'
+import fetchMock from 'fetch-mock'
 import { applyMiddleware, createStore } from 'redux'
 
 import { apiMiddleware, apiReducer } from '../src/index'
 
-const store = createStore(apiReducer, applyMiddleware(apiMiddleware))
+test.beforeEach(t => {
+  // Create a fresh store for each test.
+  t.context.store = createStore(apiReducer({
+    types: ['API_FETCH_PENDING', 'API_FETCH_SUCCESS', 'API_FETCH_ERROR']
+  }), applyMiddleware(apiMiddleware))
+})
+
+test.afterEach(t => {
+  // Reset fetch mock state after each test
+  fetchMock.reset()
+})
+
+test.after(t => {
+  // Restore original fetch function
+  fetchMock.restore()
+})
 
 test('Initializes state', t => {
+  const { store } = t.context
   t.deepEqual(store.getState(), {
     error: null,
     isPending: false,
