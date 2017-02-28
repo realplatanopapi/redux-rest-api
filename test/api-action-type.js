d6a13f0dc6e96568b5f2d96ab2d4f1860d99c9ce
Mario Pabon <me@mariopabon.com>
2017-02-28 15:28:11 -0500

    Remove existing tests

--- test/api-action-type.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava'
-
-import { API_ACTION_TYPE } from '../src'
-
-test('Unique API_ACTION_TYPE value', t => {
-  t.is(API_ACTION_TYPE, '@@API_ACTION_TYPE')
-})
--- test/package.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-import { API_ACTION_TYPE, apiMiddleware, configureApiReducer } from '../src'
-
-test('Package exports', t => {
-  t.true(typeof API_ACTION_TYPE === 'string')
-  t.true(typeof apiMiddleware === 'function')
-  t.true(typeof configureApiReducer === 'function')
-})
